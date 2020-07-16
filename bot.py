45d467fc786a375d8d4d1ae26a8c1fd734861a3a
Henry Haustein <henrydatei@web.de>
2020-07-16 17:38:21 +0200

    fixed something that I dont understand

--- bot.py
+++ bot.py
@@ -29,92 +29,88 @@ print "Crawl for information"
 crawlForInformation(token, masterID, masterName)
 
 # alts
-try:
-    file = open("alts.txt")
-    alts = file.read().splitlines()
-    file.close()
-    randomAlt = random.randint(0,len(alts)-1)
-    username = alts[randomAlt].split(":")[0]
-    password = alts[randomAlt].split(":")[1]
-    folderName = username.split("@")[0]
-    print "Use alt: " + str(username)
+file = open("alts.txt")
+alts = file.read().splitlines()
+file.close()
+randomAlt = random.randint(0,len(alts)-1)
+username = alts[randomAlt].split(":")[0]
+password = alts[randomAlt].split(":")[1]
+folderName = username.split("@")[0]
+print "Use alt: " + str(username)
 
-    # download documents
-    added = getAdditionsFromFiles(folderName + "/downloadedDocuments.txt", "documents.txt")
-    print 'not downloaded documents:'
-    for line in added:
-        print line
-    #select 2 un-downloaded documents to download
-    choice = random.sample(added, 2)
-    #download the documents
-    tokenAlt = login(username, password)
-    print "Downloading documents:"
-    f = open(folderName + "/downloadedDocuments.txt", "a+")
-    for docID in choice:
-        print docID
-        document = getDocument(docID, tokenAlt)
-        f.write(str(docID) + "\n")
-    f.close()
+# download documents
+added = getAdditionsFromFiles(folderName + "/downloadedDocuments.txt", "documents.txt")
+print 'not downloaded documents:'
+for line in added:
+    print line
+#select 2 un-downloaded documents to download
+choice = random.sample(added, 2)
+#download the documents
+tokenAlt = login(username, password)
+print "Downloading documents:"
+f = open(folderName + "/downloadedDocuments.txt", "a+")
+for docID in choice:
+    print docID
+    document = getDocument(docID, tokenAlt)
+    f.write(str(docID) + "\n")
+f.close()
 
-    # upvote Documents
-    added = getAdditionsFromFiles(folderName + "/upvotedDocuments.txt", "documents.txt")
-    print 'not upvoted documents:'
-    for line in added:
-        print line
-    #select 2 un-upvoted documents to upvote
-    choice = random.sample(added, 2)
-    #upvote the documents
-    tokenAlt = login(username, password)
-    print "Upvoting documents:"
-    f = open(folderName + "/upvotedDocuments.txt", "a+")
-    for docID in choice:
-        print docID
-        document = upvoteDocument(tokenAlt, docID)
-        f.write(str(docID) + "\n")
-    f.close()
+# upvote Documents
+added = getAdditionsFromFiles(folderName + "/upvotedDocuments.txt", "documents.txt")
+print 'not upvoted documents:'
+for line in added:
+    print line
+#select 2 un-upvoted documents to upvote
+choice = random.sample(added, 2)
+#upvote the documents
+tokenAlt = login(username, password)
+print "Upvoting documents:"
+f = open(folderName + "/upvotedDocuments.txt", "a+")
+for docID in choice:
+    print docID
+    document = upvoteDocument(tokenAlt, docID)
+    f.write(str(docID) + "\n")
+f.close()
 
-    # upvote and play Flashcards
-    added = getAdditionsFromFiles(folderName + "/upvotedFlashcards.txt", "flashcards.txt")
-    print 'not upvoted documents:'
-    for line in added:
-        print line
-    #select 2 un-upvoted flashcards to upvote
-    choice = random.sample(added, 2)
-    #upvote the flashcards
-    tokenAlt = login(username, password)
-    print "Upvoting flashcards:"
-    f = open(folderName + "/upvotedFlashcards.txt", "a+")
-    for flashcardID in choice:
-        print flashcardID
-        flashcard = upvoteFlashcard(tokenAlt, flashcardID)
-        f.write(str(flashcardID) + "\n")
+# upvote and play Flashcards
+added = getAdditionsFromFiles(folderName + "/upvotedFlashcards.txt", "flashcards.txt")
+print 'not upvoted and played flashcards:'
+for line in added:
+    print line
+#select 2 un-upvoted flashcards to upvote
+choice = random.sample(added, 2)
+#upvote the flashcards
+tokenAlt = login(username, password)
+print "Upvoting and playing flashcards:"
+f = open(folderName + "/upvotedFlashcards.txt", "a+")
+for flashcardID in choice:
+    print flashcardID
+    flashcard = upvoteFlashcard(tokenAlt, flashcardID)
+    f.write(str(flashcardID) + "\n")
 
-        # play flashcard
-        startFlashcard(tokenAlt,flashcardID)
-        finishFlashcard(tokenAlt,flashcardID)
-    f.close()
+    # play flashcard
+    startFlashcard(tokenAlt,flashcardID)
+    finishFlashcard(tokenAlt,flashcardID)
+f.close()
 
-    # upvote Questions
-    # Currently searching for an API-method. Maybe /api/app/v1/questions/upvote ?
+# upvote Questions
+# Currently searching for an API-method. Maybe /api/app/v1/questions/upvote ?
 
-    # upvote Answers
-    added = getAdditionsFromFiles(folderName + "/upvotedAnswers.txt", "answers.txt")
-    print 'not upvoted answers:'
-    for line in added:
-        print line
-    #select 2 un-upvoted answers to upvote
-    choice = random.sample(added, 2)
-    #upvote the answers
-    tokenAlt = login(username, password)
-    print "Upvoting answers:"
-    f = open(folderName + "/upvotedAnswers.txt", "a+")
-    for toUpvote in choice:
-        print toUpvote
-        type = toUpvote.split("|")[0]
-        id = toUpvote.split("|")[1]
-        answer = upvoteAnswer(tokenAlt, type, id)
-        f.write(str(toUpvote) + "\n")
-    f.close()
-except IOError:
-    print "alts.txt not found"
-    print "Please run add-alt.py to add alts"
+# upvote Answers
+added = getAdditionsFromFiles(folderName + "/upvotedAnswers.txt", "answers.txt")
+print 'not upvoted answers:'
+for line in added:
+    print line
+#select 2 un-upvoted answers to upvote
+choice = random.sample(added, 2)
+#upvote the answers
+tokenAlt = login(username, password)
+print "Upvoting answers:"
+f = open(folderName + "/upvotedAnswers.txt", "a+")
+for toUpvote in choice:
+    print toUpvote
+    type = toUpvote.split("|")[0]
+    id = toUpvote.split("|")[1]
+    answer = upvoteAnswer(tokenAlt, type, id)
+    f.write(str(toUpvote) + "\n")
+f.close()
